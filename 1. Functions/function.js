dc7ce14c726decd8732e690df353adac498694f2
Ramchandra Regmi <46373964+rregmi1993@users.noreply.github.com>
2022-05-31 23:34:33 +0530

    function types

---
+++ 1. Functions/function.js
@@ -0,0 +1,20 @@
+//normal function 
+const getName = function(fName, lName) {
+    return '${fName} ${lName}';
+};
+
+
+//Arrow Function
+const getName = function(fName, lName) => {
+    return '${fName} ${lName}';
+};
+
+// Arrow Function with No return 
+const getName = function(fName, lName) => {'${fName} ${lName}'};
+
+
+//Generator 
+function* getFirstName(){
+    yield "Ram"
+}
+
